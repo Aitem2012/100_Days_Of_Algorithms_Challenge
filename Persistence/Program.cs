c78a1ba5a8b107f3ea6d362e3fe3c0c23cff717f
Ibrahim Temitope Ajumoko <ajumokoibrahimtemitope@gmail.com>
2021-04-21 23:41:36 +0100

    Add files via upload
    Day 2 challenge

---
+++ Persistence/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Persistence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Persistence(10));
+        }
+
+        public static int Persistence(long n)
+        {
+            if(n < 10) return 0;
+            var counter =0;
+            double multiplier = 1;
+            while (!(n<10))
+            {
+                var newNum = n.ToString(); //string[] ascii 2,3
+                foreach (var item in newNum)
+                {
+                    multiplier *= Char.GetNumericValue(item);
+                }
+                ++counter;
+                n = Convert.ToInt32(multiplier); 
+                multiplier = 1;
+            }
+
+
+            return counter;
+        }
+    }
+}
---
+++ RepeatStr/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace RepeatStr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(repeatStr(5, "Hello"));
+        }
+
+        public static String repeatStr(int repeat, string strng)
+        {
+            
+            return string.Concat(Enumerable.Repeat(strng, repeat));
+        }
+    }
+}
