ff5d8e470501874a928dc77d2f1055c1ef167381
Ibrahim Temitope Ajumoko <ajumokoibrahimtemitope@gmail.com>
2021-04-22 23:35:46 +0100

    Add files via upload
    Day 3 ---------------

---
+++ FindUnique/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FindUnique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetUnique(new List<int> { 1, 1, 1, 2, 1, 1}));
+        }
+        public static int GetUnique(IEnumerable<int> numbers)
+        {
+            //your code
+            var myDict = new Dictionary<int, int>();
+            var uniqueNumbers = numbers.Distinct(); // myDict <1, 5> <2, 1>
+            foreach (var item in uniqueNumbers)
+            {
+            var counter = 0;
+                foreach (var num in numbers)
+                {
+                    if (item == num)
+                    {
+                        counter++;
+                    }
+                }
+                myDict.Add(item, counter);
+            }
+
+            var different = myDict.Values.Min();
+            foreach (var item in myDict)
+            {
+                if (item.Value == different)
+                {
+                    return item.Key;
+                }
+            }
+
+            return 0;
+        }
+    }
+}
