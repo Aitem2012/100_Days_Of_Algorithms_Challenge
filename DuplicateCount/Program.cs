90ee21e275584a07ad4c2cb860803282e1dffb90
Ibrahim Temitope Ajumoko <ajumokoibrahimtemitope@gmail.com>
2021-04-24 23:58:36 +0100

    Add files via upload
    Day 5----------

---
+++ DuplicateCount/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace DuplicateCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public static int DuplicateCount(string str)
+        {
+            var myStr = str.ToLower().Distinct();
+            var count = 0;
+            foreach (var item in myStr)
+            {
+                var counter = 0;
+                foreach (var item1 in str.ToLower())
+                {
+                    if (item == item1)
+                    {
+                        counter += 1;
+                    }
+                }
+                if (counter > 1)
+                {
+                    count +=1;
+                }
+            }
+
+
+            return count;
+        }
+    }
+}
---
+++ SumMix/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SumMix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public static int SumMix(object[] x)
+        {
+            var sum =0;
+            foreach (var item in x)
+            {
+                sum += (int)item;
+            }
+            return sum;
+        }
+    }
+}
