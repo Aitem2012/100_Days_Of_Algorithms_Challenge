e49f1445e64fa5e258f91b86e412c974577bad1b
Ibrahim Temitope Ajumoko <ajumokoibrahimtemitope@gmail.com>
2021-04-25 23:44:07 +0100

    Add files via upload
    Day 7

---
+++ ChooseBestSum/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ChooseBestSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static int? chooseBestSum(int t, int k, List<int> ls)
+        {
+            // your code
+            var myArr = new List<int>();
+            var myHashset = new HashSet<int[]>();
+
+            for (int i = 0; i < k; i++)
+            {
+                for (int j = 0; j < ls.Count; j++)
+                {
+                    for (int l = 0; l < ls.Count; l++)
+                    {
+                        
+                    }
+                }
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ CuboidVolume/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CuboidVolume
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public static int FindDifference(int[] a, int[] b)
+        {
+            //loading...
+            var aMult = 1;
+            var bMult = 1;
+            foreach (var num in a)
+                aMult *= num;
+            foreach (var num in b)
+                bMult *= num;
+            return Math.Abs(aMult - bMult);
+        }
+    }
+}
